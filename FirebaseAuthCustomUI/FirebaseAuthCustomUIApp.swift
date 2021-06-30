4dd3e13bced076f1a397da42bddb09df748f7b98
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2021-06-30 14:02:13 -0700

    Initial Commit

---
+++ FirebaseAuthCustomUI/FirebaseAuthCustomUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthCustomUIApp.swift
+//  FirebaseAuthCustomUI
+//
+//  Created by Darrick_Truong on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthCustomUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
